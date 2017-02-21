a7c9eea216cf0fbf0453f5f8d36fbfb51c3c54bc
goellnerphillip <goellner.phill-tit15@it.dhbw-ravensburg.de>
2017-02-21 14:53:28 +0100

    Basic class structure and method stubs

---
+++ src/main/java/cholesky/Cholesky.java
@@ -0,0 +1,13 @@
+package main.java.cholesky;
+
+/**
+ * Created by phillip.goellner on 21.02.2017.
+ */
+public class Cholesky {
+    public Cholesky() {
+    }
+
+    public Matrix loese(Matrix a, Matrix b) {
+        return a;
+    }
+}
--- src/main/java/cholesky/Matrix.java
+++ src/main/java/cholesky/Matrix.java
@@ -4,4 +4,43 @@ package main.java.cholesky;
  * Created by phillip.goellner on 21.02.2017.
  */
 public class Matrix {
+    public Matrix(String dateiname) {
+    }
+
+    public Matrix(int zeilen, int spalten) {
+    }
+
+    public void setElement(int zeile, int spalte, double wert) {
+    }
+
+    public double getElement(int zeile, int spalte) {
+        return 0.0;
+    }
+
+    public double[] zeile(int _zeile) {
+        return null;
+    }
+
+    public double[] spalte(int _spalte) {
+        return null;
+    }
+
+    public int hoehe() {
+        return 0;
+    }
+
+    public int breite() {
+        return 0;
+    }
+
+    public String toString() {
+        return "";
+    }
+
+    public void toFile(String dateiname) {
+    }
+
+    public Matrix transponiere() {
+        return this;
+    }
 }

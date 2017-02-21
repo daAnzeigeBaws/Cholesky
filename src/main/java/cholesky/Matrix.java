a1167f9e9c854c187f46ad9829428e31d0a2ecc8
goellnerphillip <goellner.phill-tit15@it.dhbw-ravensburg.de>
2017-02-21 16:20:41 +0100

    Implemented the file operations and the toString method

--- src/main/java/cholesky/Matrix.java
+++ src/main/java/cholesky/Matrix.java
@@ -1,5 +1,11 @@
 package main.java.cholesky;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by phillip.goellner on 21.02.2017.
  */
@@ -11,7 +17,28 @@ class Matrix {
 
     private double[][] feld;
 
-    public Matrix(String dateiname) {
+    public Matrix(String dateiname) throws IOException {
+        LinkedList<Double> values = new LinkedList();
+        List<String> ss = Files.readAllLines(Paths.get(dateiname));
+
+        String[] _tokens = ss.get(0).split(" ");
+        zeilen = Integer.parseInt(_tokens[0]);
+        spalten = Integer.parseInt(_tokens[1]);
+
+        feld = new double[zeilen][spalten];
+
+        Files.readAllLines(Paths.get(dateiname)).subList(1, ss.size()).forEach(line -> {
+            String[] tokens = line.split(" ");
+            for (String value : tokens) {
+                values.add(Double.parseDouble(value));
+            }
+        });
+
+        for (int i = 0; i < feld[0].length; i++) {
+            for (int j = 0; j < feld.length; j++) {
+                feld[j][i] = values.pop();
+            }
+        }
     }
 
     public Matrix(int zeilen, int spalten) {
@@ -44,10 +71,23 @@ class Matrix {
     }
 
     public String toString() {
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < feld[0].length; i++) {
+            for (int j = 0; j < feld.length - 1; j++) {
+                sb.append(feld[j][i]).append(' ');
+            }
+            sb.append(feld[feld.length - 1][i]).append('\n');
+        }
+        return sb.toString();
     }
 
-    public void toFile(String dateiname) {
+    public void toFile(String dateiname) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append(zeilen).append(' ').append(spalten).append('\n');
+        sb.append(this.toString());
+        sb.deleteCharAt(sb.length() - 1);
+
+        Files.write(Paths.get(dateiname), sb.toString().getBytes());
     }
 
     public Matrix transponiere() {

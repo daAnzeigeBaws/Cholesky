99bd62ab5fc7b4dc3c4a325c60d8845d35212080
goellnerphillip <goellner.phill-tit15@it.dhbw-ravensburg.de>
2017-02-22 08:17:41 +0100

    The make file to actually fulfill the requirements

--- make.bat
+++ make.bat
@@ -1,2 +1,11 @@
 @echo off
-jar -Mfc cholesky.jar src/cholesky
+del cholesky.jar
+javac src/cholesky/*.java
+if errorlevel 1 (
+    echo "Javac is not among your Environment Variables!"
+    echo "Please insert JAVA_HOME to your Path"
+)
+cd src
+jar -fc cholesky.jar cholesky/*.class
+del /S *.class
+move /Y cholesky.jar ../cholesky.jar
